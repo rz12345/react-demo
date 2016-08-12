0e9d55b4a15b39b9ac165673eaff3caf142e8bb3
Rz Chen <rz12345@gmail.com>
2016-08-12 17:46:52 +0800

    原始碼統一移至 gh-pages https://github.com/rz12345/react-demo/tree/gh-pages

---
+++ readme.md
@@ -0,0 +1,4 @@
+# react-demo
+
+原始碼統一移至 gh-pages
+https://github.com/rz12345/react-demo/tree/gh-pages
--- todos/src/entry.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import {render} from 'react-dom'
-
-const App = require('./components/App.jsx');
-
-render(
-    <App/>, document.getElementById('app'));
